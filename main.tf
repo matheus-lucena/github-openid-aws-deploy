08e1d8286b4a1b737a308176502346ccc0ea2d9a
Matheus Giovanni Lucena <contato@matheuslucena.dev>
2024-03-13 15:26:10 -0300

    remove

--- main.tf
+++ main.tf
@@ -57,14 +57,3 @@ resource "aws_api_gateway_integration_response" "ping_integration_response" {
     })
   }
 }
-
-resource "aws_api_gateway_method_response" "ping_method_response" {
-  rest_api_id = aws_api_gateway_rest_api.main.id
-  resource_id = aws_api_gateway_resource.ping_resource.id
-  http_method = aws_api_gateway_method.ping_method.http_method
-  status_code = "200"
-
-  response_models = {
-    "application/json" = "Empty"
-  }
-}
