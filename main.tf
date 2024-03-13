d181fd096b5ba047a40891161d1da3f6295266ca
Matheus Giovanni Lucena <contato@matheuslucena.dev>
2024-03-13 15:13:50 -0300

    stage $default

--- main.tf
+++ main.tf
@@ -6,6 +6,24 @@ resource "aws_api_gateway_rest_api" "main" {
   name = var.name
 }
 
+resource "aws_api_gateway_deployment" "main" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.main.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "main" {
+  deployment_id = aws_api_gateway_deployment.main.id
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  stage_name    = "$default"
+}
+
 resource "aws_api_gateway_resource" "ping_resource" {
   rest_api_id = aws_api_gateway_rest_api.main.id
   parent_id   = aws_api_gateway_rest_api.main.root_resource_id
